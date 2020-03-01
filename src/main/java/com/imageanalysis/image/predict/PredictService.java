5fb863de1a7c03fe95911bdb4e269078091c7fcc
JeremJ <jeremiasz.jaworski@bluesoft.com>
2020-03-01 21:49:07 +0100

    Integration with Prediction API

--- src/main/java/com/imageanalysis/image/predict/PredictService.java
+++ src/main/java/com/imageanalysis/image/predict/PredictService.java
@@ -1,39 +1,27 @@
 package com.imageanalysis.image.predict;
 
-import org.datavec.image.loader.ImageLoader;
-import org.deeplearning4j.nn.modelimport.keras.exceptions.InvalidKerasConfigurationException;
-import org.deeplearning4j.nn.modelimport.keras.exceptions.UnsupportedKerasConfigurationException;
-import org.nd4j.linalg.api.ndarray.INDArray;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.Resource;
+import com.imageanalysis.exception.ProcessingFileException;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
-import static org.deeplearning4j.nn.modelimport.keras.KerasModelImport.importKerasSequentialModelAndWeights;
 
 @Service
 public class PredictService {
-    public static final String MODEL_FILE_CLASSPATH = "classpath:Lung-model.h5";
 
-    private Resource modelFile;
-
-    public PredictService(@Value(MODEL_FILE_CLASSPATH) Resource modelFile) {
-        this.modelFile = modelFile;
-    }
-
+    public static final String PREDICTION_API = "http://localhost:8000/api/v1/predictions";
     public Double predict(MultipartFile multipartFile) {
-        var result = 0d;
+        RestTemplate restTemplate = new RestTemplate();
         try {
-            var model = importKerasSequentialModelAndWeights(modelFile.getInputStream(), true);
-            INDArray ads = new ImageLoader().asMatrix(multipartFile.getInputStream());
-            var ss = model.output(ads);
-            result = 2d;
-        } catch (IOException | InvalidKerasConfigurationException | UnsupportedKerasConfigurationException e) {
-            e.printStackTrace();
+            ResponseEntity<Double> exchange = restTemplate
+                    .postForEntity(PREDICTION_API, multipartFile.getBytes(), Double.class);
+            return exchange.getBody();
+        } catch (IOException e) {
+            throw new ProcessingFileException("dupa");
         }
-        return result;
     }
 
 
